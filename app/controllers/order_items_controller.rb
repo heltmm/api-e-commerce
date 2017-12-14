88ee00417c7ae75934c0c625eaa39d01a79184f1
Mark Helt <heltmm@gmail.com>
2017-12-14 13:52:29 -0800

    remove prys

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -10,7 +10,6 @@ class OrderItemsController < ApplicationController
   end
 
   def destroy
-    binding.pry
     @order = current_order
     @item = @order.order_items.find(params[:id])
     @item.destroy
